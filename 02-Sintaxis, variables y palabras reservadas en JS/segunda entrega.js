ab3f2fee967a837b09e550852576bf61e6bf1548
TomyGauna <tgauna99@gmail.com>
2023-05-04 11:56:20 -0300

    Create segunda entrega.js

---
+++ 02-Sintaxis, variables y palabras reservadas en JS/segunda entrega.js
@@ -0,0 +1,14 @@
+const nombre = "Tomás L. Gauna";
+
+let edad = 24;
+
+desarrollador = true;
+
+const fechaNacimiento = new Date("13 march 1999");
+
+const libro = {
+  titulo: "Martín Fierro",
+  autor: "José Hernández",
+  fecha: 1872,
+  url: "https://www.amazon.com/-/es/José-Hernández-ebook/dp/B07LC1RWT5/ref=sr_1_2?__mk_es_US=ÅMÅŽÕÑ&crid=13RBAXQSMIW3M&keywords=martin+fierro&qid=1683212065&s=books&sprefix=martin+fierro%2Cstripbooks-intl-ship%2C246&sr=1-2",
+};
